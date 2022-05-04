b2de3812d0ac24f8829a7ee948f51b68a7739a24
elizjo <eli3jo.art@gmail.com>
2022-05-04 04:08:26 -0700

    fixed bugs with opening model

--- pages/mlp_model.py
+++ pages/mlp_model.py
@@ -4,8 +4,9 @@ import streamlit as st
 @st.cache()
 def prediction(seq_len, mole_w, gravy, instab, aromat):
     # loading the trained model
-    pickle_in = open('data,model/prot_classifier.pkl', 'rb') 
-    classifier = pickle.load(pickle_in)
+    with open(r"data,model/prot_classifier.pkl", 'rb') as input_file: 
+    # pickle_in = open('data,model/prot_classifier.pkl', 'rb') 
+        classifier = pickle.load(input_file)
     # Making predictions 
     prediction = classifier.predict( 
         [[seq_len, mole_w, gravy, instab, aromat]])
