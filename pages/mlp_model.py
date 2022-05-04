2cb65926f9b5458de452c18f49b4e1f095e223b6
elizjo <eli3jo.art@gmail.com>
2022-05-04 04:14:22 -0700

    bugs on input file

--- pages/mlp_model.py
+++ pages/mlp_model.py
@@ -7,9 +7,12 @@ def prediction(seq_len, mole_w, gravy, instab, aromat):
     with open(r"data,model/prot_classifier.pkl", 'rb') as input_file: 
     # pickle_in = open('data,model/prot_classifier.pkl', 'rb') 
         classifier = pickle.load(input_file)
-    # Making predictions 
-    prediction = classifier.predict( 
-        [[seq_len, mole_w, gravy, instab, aromat]])
+    
+        # Making predictions 
+        prediction = classifier.predict( 
+            [[seq_len, mole_w, gravy, instab, aromat]])
+        
+        input_file.close()
      
     if prediction == 1:
         pred = 'Disordered'
