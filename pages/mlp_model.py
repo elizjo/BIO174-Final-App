4515418b4fc1d9eb9b877445c8894d00929136fe
elizjo <eli3jo.art@gmail.com>
2022-05-04 04:17:02 -0700

    Update mlp_model.py

--- pages/mlp_model.py
+++ pages/mlp_model.py
@@ -4,7 +4,7 @@ import streamlit as st
 @st.cache()
 def prediction(seq_len, mole_w, gravy, instab, aromat):
     # loading the trained model
-    with open(r"data,model/prot_classifier.pkl", 'rb') as input_file: 
+    with open(r"data,model/prot_classifier.pkl", 'wb') as input_file: 
     # pickle_in = open('data,model/prot_classifier.pkl', 'rb') 
         classifier = pickle.load(input_file)
     
